fb27024032c23b053d13aaedba3916ae3094851d
zhengMa <z1399956473@gmail.com>
2023-10-05 13:53:13 +0800

    lsp compile function finished

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,7 +1,124 @@
-import {add} from "../src";
+import {CompilationInfo, compile, compileAll} from "../src";
 
-describe("First tests!!!", () => {
-    it("should return 3", () => {
-        expect(add(1, 2)).toEqual(3);
-    })
+describe("Compile tests", () => {
+    it('should return error when the keyword is not defined.', () => {
+        expect(
+            compile(`"(ImNotDefined arg1 arg2)": {req: {}, res: {}}`)[0].error
+        ).toEqual("Invalid keyword.")
+    });
+
+    it('should return error when the type/string is missing', () => {
+        expect(compile(`"(Local/get-in)":{req: {}, res: {}}`)[0].error).toEqual("Error on the 1st argument: The syntax should be type/string")
+    });
+
+    it('should return error when the url formatting mappings is missing', () => {
+        expect(compile(`"(Rest/get /users/test)":{req: {}, res: {}}`)[0].error).toEqual("Error on the 2st argument: The syntax should be one of type/map,fn/selfMappings")
+    });
+
+    it('should return error when the type/url`s formatting is missing', () => {
+        expect(compile(`"(Rest/get {} {})":{req: {}, res: {}}`)[0].error).toEqual("Error on the 1st argument: The syntax should be a url")
+    });
+
+    it('should return error when the url formatting is missing with selfMappings', () => {
+        expect(compile(`"(Rest/get /users/:id?:test selfMappings)":{req: {id: number}, res: {}}`)[0].error).toEqual("Error on the 1st argument: The keyword 'test' is not mapping")
+
+        expect(compile(`"(Rest/get /users/:id?:test selfMappings)":{req: {test:string}, res: {}}`)[0].error).toEqual("Error on the 1st argument: The keyword 'id' is not mapping")
+    });
+
+    it('should return error when the url formatting is missing with map data structure mappings', () => {
+        expect(compile(`"(Rest/get /users/:id?:test {:idx id})":{req: {id: number}, res: {}}`)[0].error).toEqual("Error on the 1st argument: The keyword 'id' is not mapping")
+
+        expect(compile(`"(Rest/get /users/:id?:test {:id idx})":{req: {id: number}, res: {}}`)[0].error).toEqual("Error on the 1st argument: The mapping 'idx' should be exists in the request data.")
+
+        expect(compile(`"(Rest/get /users/:id?:test {:id id   :testx x})":{req: {id: number}, res: {}}`)[0].error).toEqual("Error on the 1st argument: The keyword 'test' is not mapping")
+
+        expect(compile(`"(Rest/get /users/:id?:test {:id id   :test x})":{req: {id: number}, res: {}}`)[0].error).toEqual("Error on the 1st argument: The mapping 'x' should be exists in the request data.")
+
+        expect(compile(`"(Rest/get /users/:id?:test {:id id   :test x})":{req: {id: number    x: string}, res: {}}`)[0].error).toEqual(undefined)
+    });
+})
+
+describe("Compile tests on Rest/get", () => {
+    it('should return error when keyword is not match', () => {
+        expect(compile(`"(Rest/gets /users/:id?:test {:idx id})":{req: {id: number}, res: {}}`)[0].error).toEqual("Invalid keyword.")
+    });
+
+    it('should return error when the url is not an url', () => {
+        expect(compile(`"(Rest/get test {:idx id})":{req: {id: number}, res: {}}`)[0].error).toEqual("Error on the 1st argument: The syntax should be a url")
+    });
+
+    it('should return error when the url formatting mappings is missing', () => {
+        expect(compile(`"(Rest/get /users/)":{req: {id: number}, res: {}}`)[0].error).toEqual("Error on the 2st argument: The syntax should be one of type/map,fn/selfMappings")
+    });
+
+    it('should return error when the url formatting mappings is wrong', () => {
+        expect(compile(`"(Rest/get /users/ --)":{req: {id: number}, res: {}}`)[0].error).toEqual("Error on the 2st argument: The syntax should be one of type/map,fn/selfMappings")
+    });
+
+    it('should return error when the url is not an url', () => {
+        expect(compile(`"(Rest/get /users/:id/posts?test&:name {:id id :test test :name name})":{req: {id: number, test:string, name: string}, res: {}}`)[0].error).toEqual("Error on the 1st argument: The syntax should be a url")
+    });
+
+    it('should not return error when syntax is ok', () => {
+        expect(compile(`"(Rest/get /users/:id/posts?:test&:name {:id id :test test :name name})":{req: {id: number, test:string, name: string}, res: {}}`)[0].error).toEqual(undefined)
+    });
+
+    it('should not return error when syntax is ok --url', () => {
+        expect(compile(`"(Rest/get /users/ selfMappings)":{req:{id: number} res: {}}`)[0].error).toEqual(undefined)
+    });
+})
+
+describe("Compile tests on Rest/post", () => {
+    it('should throw exception when the body mappings is missing', () => {
+        expect(compile(`"(Rest/post /users/ selfMappings)":{req:{id: number} res: {}}`)[0].error).toEqual("Error on the 3st argument: The syntax should be one of type/map,fn/asBody")
+    });
+
+    it('should return error when the url formatting mappings is not correct', () => {
+        expect(compile(`"(Rest/post /users/ -- asBody)":{req:{id: number} res: {}}`)[0].error).toEqual("Error on the 2st argument: The syntax should be one of type/map,fn/selfMappings")
+    });
+
+    it('should return error when the url is not correct', () => {
+        expect(compile(`"(Rest/post test {} asBody)":{req:{id: number} res: {}}`)[0].error).toEqual("Error on the 1st argument: The syntax should be a url")
+    });
+
+    it('should return error when the url formatting mappings is missing', () => {
+        expect(compile(`"(Rest/post /users/:id/profile?:old {} asBody)":{req:{id: number} res: {}}`)[0].error).toEqual("Error on the 1st argument: The keyword 'id' is not mapping")
+    });
+
+    it('should return error when the url formatting mappings is not correct', () => {
+        expect(compile(`"(Rest/post /users/:id/profile?:old {:id idx} asBody)":{req:{id: number} res: {}}`)[0].error).toEqual("Error on the 1st argument: The mapping 'idx' should be exists in the request data.")
+    });
+
+    it('should not error error when the syntax is ok', () => {
+        expect(compile(`"(Rest/post /users/:id/profile?:old selfMappings asBody)":{req:{id: number, old:boolean} res: {}}`)[0].error).toEqual(undefined)
+    });
+})
+
+describe("Compile tests on Local/get-in", () => {
+    it('should should return error when the key is missing', () => {
+        expect(compile(`"(Local/get-in)":{req:{id: number, old:boolean} res: {}}`)[0].error).toEqual("Error on the 1st argument: The syntax should be type/string")
+    });
+
+    it('should not return error when the syntax is correct', () => {
+        expect(compile(`"(Local/get-in user-profile)":{req:{id: number, old:boolean} res: {}}`)[0].error).toEqual(undefined)
+    });
+})
+
+describe("Compile tests on Local/set-in", () => {
+    it('should should return error when the key is missing', () => {
+        expect(compile(`"(Local/set-in)":{req:{id: number, old:boolean} res: {}}`)[0].error).toEqual("Error on the 1st argument: The syntax should be type/string")
+    });
+
+    it('should not return error when the syntax is correct', () => {
+        expect(compile(`"(Local/set-in user-profile)":{req:{id: number, old:boolean} res: {}}`)[0].error).toEqual(undefined)
+    });
+})
+
+
+describe("CompileAll Tests", () => {
+    it('should throw exception when the data access layer not exists', () => {
+        expect(() => {
+            compileAll()
+        }).toThrow("ERROR: Add data access layer to your project root folder before compiling. More information: xxx");
+    });
 })
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,226 @@
-export function add(a: number, b: number): number {
-    return a + b;
+import * as fs from 'fs'
+import * as path from "path";
+import {loadApiMetaDataFromTheSourceCode} from './apiMetadata'
+
+const keywords: { [key: string]: string[] } = {
+    "Rest/get": ["(type/url)", "(one-of type/map fn/selfMappings)"],
+    "Rest/post": ["(type/url)", "(one-of type/map fn/selfMappings)", "(one-of type/map fn/asBody)"],
+    "Rest/delete": ["(type/url)", "(one-of type/map fn/selfMappings)", "(one-of type/map fn/asBody)"],
+    "Rest/put": ["(type/url)", "(one-of type/map fn/selfMappings)", "(one-of type/map fn/asBody)"],
+    "Rest/patch": ["(type/url)", "(one-of type/map fn/selfMappings)", "(one-of type/map fn/asBody)"],
+    "Local/get-in": ["(type/string)"],
+    "Local/set-in": ["(type/string)"]
 }
 
-console.log(`The results? ${add(1, 2)}`)
+export type CompilationInfo = {
+    lsp: string,
+    args: string[],
+    error: string | undefined
+}
+
+class ValidationFactContext {
+    public bodyMappings?: { [key: string]: string };
+    public urlFormattingMappings?: { [key: string]: string };
+    public reqDataKeys: string[]
+
+    constructor(reqDataKeys: string[]) {
+        this.reqDataKeys = reqDataKeys;
+    }
+}
+
+function checkIfTheSyntaxAValidUrl(syntax: string):boolean {
+   if (syntax == null || !syntax.startsWith("/")) {
+       return false;
+   }
+
+   const queryParameter = syntax.split("?")[1];
+   if (queryParameter == undefined) {
+       return true;
+   }
+
+   const args = queryParameter.split("&");
+   for (const arg of args) {
+       if (!arg.startsWith(":")) {
+           return false;
+       }
+   }
+
+
+
+
+   return true;
+}
+
+const syntaxValidators: {
+    [key: string]: (syntax: string | null, args: string[], facts: ValidationFactContext) => string | undefined
+} = {
+    "type/url": (syntax, args, facts): string | undefined => {
+        if (syntax == null || !checkIfTheSyntaxAValidUrl(syntax)) {
+            return "The syntax should be a url"
+        }
+        // Initialize a set to hold the keywords
+        let keywords: Set<string> = new Set();
+
+        // Define the regular expression
+        let pattern: RegExp = /:(\w+)/g;
+        let match: RegExpExecArray | null;
+        // Find all matches in the current URL
+        while ((match = pattern.exec(syntax)) !== null) {
+            // Add the matched keyword to the set
+            keywords.add(match[1]);
+        }
+
+        console.log(`keywords? ${Array.from(keywords)} urlFormatting? ${JSON.stringify(facts.urlFormattingMappings)}`)
+        for (const keyword of keywords) {
+            const mapping = facts.urlFormattingMappings?.[keyword];
+            if (mapping == null) {
+                return `The keyword '${keyword}' is not mapping`
+            }
+
+            if (facts.reqDataKeys.indexOf(mapping) <= -1) {
+                return `The mapping '${mapping}' should be exists in the request data.`
+            }
+        }
+    },
+    "type/string": (syntax, args, facts): string | undefined => {
+        if (syntax == null) {
+            return "The syntax should be type/string"
+        }
+        return undefined;
+    },
+    "type/map": (syntax, args, facts): string | undefined => {
+        console.log(`syntax? ${syntax}`)
+        if (syntax == null || !/^\{\s*(:\w+\s+\w+\s*)*\}$/.test(syntax)) {
+            return "The syntax should be a map in clojure way."
+        }
+
+        let mappings: { [key: string]: string } = {};
+        let pairs: string[] = syntax.slice(1, -1).trim().split(/\s+(?=:)/);
+        for (let pair of pairs) {
+            let [key, value] = pair.trim().split(/\s+/);
+            mappings[key.slice(1)] = value;
+        }
+        facts.urlFormattingMappings = mappings;
+        facts.bodyMappings = mappings;
+        return undefined
+    },
+    "one-of": (syntax, args, facts): string | undefined => {
+        console.log(`args? ${args}`)
+        for (const option of args) {
+            const error = syntaxValidators[option](syntax, [], facts)
+            if (error == undefined) {
+                return undefined;
+            }
+        }
+
+        return `The syntax should be one of ${args.toString()}`
+    },
+    "fn/selfMappings": (syntax, args, facts): string | undefined => {
+        if (syntax == null || syntax !== "selfMappings") {
+            return "The syntax should be the keyword: selfMappings"
+        }
+
+        facts.urlFormattingMappings = facts.reqDataKeys.reduce((rs, key) => {
+            rs[key] = key;
+            return rs;
+        }, {} as {[key: string]: string})
+
+        return undefined;
+    },
+    "fn/asBody": (syntax, args, facts): string | undefined => {
+        if (syntax == null || syntax !== 'asBody') {
+            return "The syntax should be the keyword: asBody"
+        }
+
+        facts.bodyMappings = facts.reqDataKeys.reduce((rs, key) => {
+            rs[key] = key;
+            return rs;
+        }, {} as {[key: string]: string})
+        return undefined;
+    }
+}
+
+export function lspToArray(lsp: string): string[] {
+    const stringsWithoutParentThese = lsp.slice(1, -1);
+    return stringsWithoutParentThese.split(/ (?![^{]*})/).filter((str) => {
+        return str.trim().length > 0;
+    });
+}
+
+export function compile(sourceCode: string): CompilationInfo[] {
+    const metaDataArr = loadApiMetaDataFromTheSourceCode(sourceCode);
+    const compilations: CompilationInfo[] = [];
+    for (const metadata of metaDataArr) {
+        const [keyword, ...args] = lspToArray(metadata.lspDef)
+        const syntaxTemplate = keywords[keyword];
+        if (syntaxTemplate == undefined) {
+            compilations.push({
+                lsp: metadata.lspDef,
+                args: metadata.args,
+                error: "Invalid keyword."
+            })
+            break;
+        }
+
+        const facts = new ValidationFactContext(metadata.args)
+        for (let i = syntaxTemplate.length - 1; i > -1; --i) {
+            const syntax = args.length > i ? args[i] : null;
+            const template = syntaxTemplate[i];
+            const [keywordOfTheTemplate, ...argsOfTheTemplate] = lspToArray(template);
+
+
+            const validator = syntaxValidators[keywordOfTheTemplate];
+            const error = validator(syntax, argsOfTheTemplate, facts);
+            if (error) {
+                compilations.push({
+                    lsp: metadata.lspDef,
+                    args: metadata.args,
+                    error: `Error on the ${i + 1}st argument: ${error}`
+                })
+                break;
+            }
+        }
+        compilations.push({
+            lsp: metadata.lspDef,
+            args: metadata.args,
+            error: undefined
+        })
+    }
+
+
+    return compilations;
+}
+
+function getTsFiles(dir: string, fileList: string[] = []): string[] {
+    const files = fs.readdirSync(dir);
+
+    files.forEach((file) => {
+        if (fs.statSync(path.join(dir, file)).isDirectory()) {
+            fileList = getTsFiles(path.join(dir, file), fileList);
+        } else if (path.extname(file) === '.ts') {
+            fileList.push(path.join(dir, file));
+        }
+    });
+
+    return fileList;
+}
+
+export function compileAll() {
+    const apiPath = process.cwd() + "/data/api";
+    const exists = fs.existsSync(apiPath);
+    if (!exists) {
+        throw "ERROR: Add data access layer to your project root folder before compiling. More information: xxx";
+    }
+    const tsFilesPath = getTsFiles(apiPath, []);
+    tsFilesPath.forEach(file => {
+        const sourceCode = fs.readFileSync(file, 'utf-8')
+        const compilations = compile(sourceCode)
+        const error = compilations.filter(compilation => {
+            return compilation.error !== undefined;
+        })[0];
+
+        if (error) {
+            throw `ERROR: ${JSON.stringify(compilations)}`
+        }
+    })
+}
